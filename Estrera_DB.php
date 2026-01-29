702c4bad9d72f1947cf2acb0b6294c22625201af
newbiee-ctrl <vincenixonestrera@gmail.com>
2026-01-29 20:00:55 +0800

    Refactor connection code in Estrera_DB.php

--- Estrera_DB.php
+++ Estrera_DB.php
@@ -5,14 +5,13 @@ ini_set('display_errors', 1);
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "usertbl"; // <-- set your database name that contains table `usertbl`
-
-// Use mysqli OO interface so `$conn->query()` in the page works reliably
+$dbname = "usertbl"; 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
 
 
 
